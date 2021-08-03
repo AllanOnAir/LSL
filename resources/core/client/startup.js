fbd34c1ce3da61973306d03e6285ad9a56b9a078
allandufour <allandufour30@gmail.com>
2021-08-03 13:57:31 -0400

    Comentaire de la database optimisation en cours

--- resources/core/client/startup.js
+++ resources/core/client/startup.js
@@ -1,5 +1,7 @@
 /// <reference types="@altv/types-client" />
 /// <reference types="@altv/types-natives" />
+/// <reference types="@altv/types-client" />
+/// <reference types="@altv/types-natives" />
 import * as alt from 'alt-client';
 import * as native from 'natives';
 import './chat';
