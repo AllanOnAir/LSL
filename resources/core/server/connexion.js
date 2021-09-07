219d9884b602fe696dfde40d78d5e4b1bed413b1
allandufour <allandufour30@gmail.com>
2021-09-07 19:16:20 -0400

    Bug fix cam at start

--- resources/core/server/connexion.js
+++ resources/core/server/connexion.js
@@ -2,7 +2,7 @@ import * as alt from 'alt-server';
 
 
 alt.onClient('charCreation', player => {
-    player.spawn(436.491, -982.172, 30.699, 1000);
+    player.spawn(436.491, -982.172, 30.699, 500);
     player.model = `mp_m_freemode_01`;
     alt.onClient("changePedSex", (player, sex) =>{
         player.model= sex;
